1d33dfdb88c70e928791cde962c74e692a6715a3
cc <953881082@qq.com>
2022-04-18 21:11:12 +0800

    first commit

---
+++ src/main/java/git/GitTest.java
@@ -0,0 +1,13 @@
+package git;
+
+/**
+ * @className: GitTest
+ * @description: TODO 类描述
+ * @author: CCQ
+ * @date: 2022/4/18
+ **/
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
